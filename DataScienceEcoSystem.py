088e6be1f6742ac799d294f3f67ed324f43f22f1
yemofest <138827813+yemofest@users.noreply.github.com>
2025-09-05 22:36:04 +0100

    Add files via upload

---
+++ DataScienceEcoSystem.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and EcoSysytem
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# ### Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Scala
+# 5. C++
+# 6. Java
+# 7. Julia
+
+# ##  Objectives
+# #### Some Key Takeaway: Some Popular Languages For Data Scientists;
+# + Python
+# + R
+# + SQL
+# + Julia
+# + C++
+# 
+# 
+
+# # Exercise 5 - Create a markdown cell to list data science libraries
+# 
+# 
+# 
+
+# ### Some of the commonly used libraries used by Data Scientists include:
+# 1. Pandas
+# 2. Matplotlib
+# 3. Seaborn
+# 4. Scikit-learn
+# 5. Keras
+# 6. TensorFlow
+# 7. PyTorch
+
+# In[1]:
+
+
+# Exercise 6 - Create a markdown cell with a table of Data Science tools
+
+
+# ### Data Science Tools
+# 
+# | Data Science Tools |
+# | Jupyter |,
+# | RStudio |,
+# | Apache Zeppelin |
+
+# In[4]:
+
+
+# Exercise 7 - Create a markdown cell introducing arithmetic expression examples
+
+# Add a line in this cell with H3 style heading with text like:
+
+
+# # Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[5]:
+
+
+# Exercise 8 - Create a code cell to multiply and add numbers
+
+# In this code cell evaluate the expression (3*4)+5.
+# Insert a comment line before the expression to explain the operation e.g.  This a simple arithmetic expression to mutiply then add integers.
+# Then execute the cell to ensure the expression returns the expected output of 17.
+
+
+# In[6]:
+
+
+# This a simple arithmetic expression to multiply then add integers.
+(3*4)+5
+
+
+# In[7]:
+
+
+# Exercise 9 - Create a code cell to convert minutes to hours
+
+# In this code cell write an expression that converts 200 minutes into hours.
+# Insert a comment line before the expression to explain the operation e.g.  This will convert 200 minutes to hours by diving by 60.
+# Run the cell to evaluate the expression.
+
+
+# In[8]:
+
+
+# This will convert 200 minutes to hours by dividing by 60.
+
+200/60
+
+
+# In[10]:
+
+
+# Exercise 10 - Insert a markdown cell to list Objectives
+
+# Below the introduction cell created in Exercise 3,
+# insert a new markdown cell to list the objectives that this notebook covered
+# (i.e. some of the key takeaways from the course). In this new cell start with an introductory line titled:
+# Objectives: in bold font. Then using an unordered list (bullets) indicate 3 to 5 items covered in this notebook,
+# such as List popular languages for Data Science'''
+
+
+# ### Objectives:
+# 
+# #### List popular languages for Data Science
+# 
+# Create markdown tables in a Jupyter Notebook
+# 
+# Use a code cell for basic arithmetic operations
+# 
+# Convert units using a code cell
+
+# In[11]:
+
+
+# Exercise 11 - Create a markdown cell to indicate the Author's name
+
+
+# ## Author: Adeyemo Adniji
+
+# In[ ]:
+
+
+
+
